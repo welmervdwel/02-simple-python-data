28d1ca7b12407509f7b01f0c371bbeb37a192e6d
Philip Gonzalez <p-gonzo@users.noreply.github.com>
2019-03-21 17:13:39 -0600

    Update simple_python_data.py

--- simple_python_data.py
+++ simple_python_data.py
@@ -1,26 +1,26 @@
 def calculate_rectangle_area(height, width):    
     ## Todo: update `None` to contain the formula for `rectangle_area`
-    rectangle_area = height * width
+    rectangle_area = None
     return rectangle_area
 
 def calculate_area_of_square(side):
     ## Todo: update `None` to contain the formula for `squre_area`
-    square_area = side ** 2
+    square_area = None
     return square_area
 
 def calculate_total_plus_tip_per_person(total_bill, tip_percent, number_of_people):
     ## Todo: update `None` to contain the formula for `total_plus_tip_` and `total_plus_tip_per_person`.
-    total_plus_tip = total_bill * (1+(tip_percent / 100))
-    total_plus_tip_per_person = total_plus_tip / number_of_people
+    total_plus_tip = None
+    total_plus_tip_per_person = None
     return total_plus_tip_per_person
 
 def fahrenheit_to_celcius(degrees):
     ## Todo: update `None` to contain the formula for `degrees_in_celcius`
     ## use the formula (F - 32) × 5/9 = C
-    degrees_in_celcius = (degrees - 32) * (5/9)
+    degrees_in_celcius = None
     return degrees_in_celcius
 
 def calculate_the_remainder(num1, num2):
     ## Todo: update `None` to contain the formula for `remainder`
-    remainder = num1 % num2
+    remainder = None
     return remainder
