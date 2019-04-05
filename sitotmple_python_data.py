074613384232055bd1050845fd0ed06fac69ef6a
welmervdwel <49283694+welmervdwel@users.noreply.github.com>
2019-04-04 18:39:34 -0600

    Update and rename simple_python_data.py to sitotmple_python_data.py

--- sitotmple_python_data.py
+++ sitotmple_python_data.py
@@ -1,26 +1,30 @@
 def calculate_rectangle_area(height, width):    
     ## Todo: update `None` to contain the formula for `rectangle_area`
-    rectangle_area = None
+    # height = int(a)
+    # width = int(b)
+    rectangle_area = height * width
     return rectangle_area
 
 def calculate_area_of_square(side):
     ## Todo: update `None` to contain the formula for `squre_area`
-    square_area = None
+    square_area = side * 2
     return square_area
 
 def calculate_total_plus_tip_per_person(total_bill, tip_percent, number_of_people):
     ## Todo: update `None` to contain the formula for `total_plus_tip_` and `total_plus_tip_per_person`.
-    total_plus_tip = None
-    total_plus_tip_per_person = None
+    total_plus_tip = total_bill * tip_percent * number_of_people
+    total_plus_tip_per_person = total_plus_tip / number_of_people
     return total_plus_tip_per_person
 
 def fahrenheit_to_celcius(degrees):
     ## Todo: update `None` to contain the formula for `degrees_in_celcius`
     ## use the formula (F - 32) × 5/9 = C
-    degrees_in_celcius = None
+    F = degrees
+    degrees_in_celcius = (F - 32) * (5/9)
     return degrees_in_celcius
 
 def calculate_the_remainder(num1, num2):
     ## Todo: update `None` to contain the formula for `remainder`
-    remainder = None
+    remainder = num1 % num2
+    # or num1 - num2???
     return remainder
